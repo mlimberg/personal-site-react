538e8e26c1bfb8391e1951f8d470a8572cab88ef
mlimberg <limbergmike@gmail.com>
2017-02-13 08:47:11 -0700

    Add image to home route

--- lib/components/home/Home.jsx
+++ lib/components/home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './home-style';
 
 const Home = () => {
   return (
-    <div>
-      HOME!
+    <div className='main-app'>
+      {/* HOME! */}
     </div>
   )
 }
