f420144e780ec2c2bce55a8202ba87ae432fe746
mlimberg <limbergmike@gmail.com>
2017-02-14 11:44:38 -0700

    Add redirect to project card on click

--- lib/components/projectCard/ProjectCard.jsx
+++ lib/components/projectCard/ProjectCard.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 import './project-card-style';
 
 const ProjectCard = (props) => {
-  const { title, url, description, img } = props;
+  const { title, url, summary, img } = props;
+
+  const redirect = () => {
+    window.open(url, '_blank')
+  }
 
   return (
-    <div className='project-card'>
-      <h2>{title}</h2>
-      <img src={img} className='project-card-img'/>
+    // <div className='tool'
+    <div className='project-card' onClick={() => redirect()}>
+      <p className='tooltip'>Click to see the project on GitHub!</p>
+      {/* <a href={url} target='_blank'> */}
+        <h2 className='project-card-title'>{title}</h2>
+        <img src={img} className='project-card-img'/>
+        <p className='project-card-summary'>{summary}</p>
+      {/* </a> */}
     </div>
   )
 }
