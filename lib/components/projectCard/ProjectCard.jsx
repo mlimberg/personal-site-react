2fc0ebac92f20537fb779a1f9e98dbbf409e2dab
mlimberg <limbergmike@gmail.com>
2017-02-15 13:12:36 -0700

    Add image toggle to headshot

--- lib/components/projectCard/ProjectCard.jsx
+++ lib/components/projectCard/ProjectCard.jsx
@@ -9,14 +9,11 @@ const ProjectCard = (props) => {
   }
 
   return (
-    // <div className='tool'
     <div className='project-card' onClick={() => redirect()}>
-      <p className='tooltip'>Click to see the project on GitHub!</p>
-      {/* <a href={url} target='_blank'> */}
-        <h2 className='project-card-title'>{title}</h2>
-        <img src={img} className='project-card-img'/>
-        <p className='project-card-summary'>{summary}</p>
-      {/* </a> */}
+      <p className='tooltip-text'>Click to see the project on GitHub!</p>
+      <h2 className='project-card-title'>{title}</h2>
+      <img src={img} className='project-card-img'/>
+      <p className='project-card-summary'>{summary}</p>
     </div>
   )
 }
