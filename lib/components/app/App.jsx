19db1ac07f708b3109f5a63ad42a2ade330ae20d
mlimberg <limbergmike@gmail.com>
2017-03-06 16:41:48 -0700

    Final response design adds

--- lib/components/app/App.jsx
+++ lib/components/app/App.jsx
@@ -17,7 +17,7 @@ export default class App extends Component {
     return (
       <div>
           <Header />
-          <div className="other">
+          <div className="sub-header">
             {Children}
           </div>
       </div>
