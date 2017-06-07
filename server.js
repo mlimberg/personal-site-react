2e6e2962d64126c50a2837e74500dca62fc50a10
Mike Limberg <mikelimberg@Mikes-MacBook-Pro.local>
2017-06-06 21:49:49 -0600

    Add server code

--- server.js
+++ server.js
@@ -3,4 +3,8 @@ var app = express();
 
 app.use(express.static(__dirname + '/'));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname + '/../dist/index.html'));
+  });
+
 app.listen(process.env.PORT || 8080);
