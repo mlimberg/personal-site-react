c9f2825206eaf123dfb9dab47d5501568cc7fd81
Mike Limberg <mikelimberg@Mikes-MacBook-Pro.local>
2017-06-06 21:32:48 -0600

    Add download resume functionality

--- lib/components/resume/Resume.jsx
+++ lib/components/resume/Resume.jsx
@@ -1,20 +1,59 @@
 import React, { Component } from 'react';
+import firebase, { storage, gsRef } from '../../firebase';
 import './resume-style';
 
+export default class Resume extends Component {
+  constructor() {
+    super();
+    this.state = {
+      resumeURL: ''
+    }
+  }
 
-const Resume = () => {
+  download() {
+    const downloadURI = (uri, name) => {
+      console.log('hit!');
+      var link = document.createElement("a");
+      link.download = name;
+      link.href = uri;
+      document.body.appendChild(link);
+      link.click();
+    }
 
-  const handleClick = () => {
+    gsRef.child('Limberg-Mike-Resume.pdf').getDownloadURL()
+    .then(url => {
+      let xhr = new XMLHttpRequest();
+
+      xhr.responseType = 'blob';
+      xhr.onload = (event) => {
+        var blob = xhr.response;
+      };
+      xhr.open('GET', url);
+      xhr.send()
+      xhr.onreadystatechange = () => {
+        if(xhr.readyState === 4) {
+          downloadURI(xhr.responseURL, 'Limberg-Mike-Resume')
+        }
+      }
+
+    })
+    .catch(err => console.error('err ', err))
+  }
+
+
+  handleClick() {
     window.open('https://resume.creddle.io/resume/7kk4iroeef0', '_blank')
   }
 
-  return (
-    <div className='resume-container'>
-        <img src='./lib/images/resume.jpg'
-             className='resume-img'
-             onClick={() => handleClick()}/>
-    </div>
-  );
-}
+  render() {
 
-export default Resume
+    return (
+      <div className='resume-container'>
+        <button className='print-resume-btn btn' onClick={() => this.download()}>Download PDF</button>
+        <img src='./lib/images/resume.jpg'
+        className='resume-img'
+        onClick={() => this.handleClick()}/>
+      </div>
+    );
+  }
+}
